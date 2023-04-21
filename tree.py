f4a9bebdba468257f3f0e0c580bb042d24ebc5e4
Kenrick Dehli <kenrickdehli@gmail.com>
2023-04-21 14:36:17 +0200

    Solved Exercise 1.

--- tree.py
+++ tree.py
@@ -1,32 +1,61 @@
 import os
 
-VERTICAL_LINE = chr(9472)
-HORIZONTAL_LINE = chr(9474)
-RIGTH_ANGLE = chr(9492)
+VERTICAL_LINE = chr(9474)
+HORIZONTAL_LINE = chr(9472)
+RIGTH_ANGLE_LINE = chr(9492)
 HORIZONTAL_VERTICAL_LINE = chr(9500)
-TAB = chr(0x20) + chr(0x20)
+TAB = chr(0x20) * 2
+SPACE = chr(0x20)
+drawVerticalLine = []
 
 
 def print_directory(path, indentation_level=0):
+    directories, files = (0, 0)
     directory = sorted(os.scandir(path), key=lambda f: f.name.lower())
     count = 1
     ending = directory.__len__()
     for item in directory:
         if (item.is_dir()):
-            print((indentation_level * HORIZONTAL_LINE) +
-                  indentation_level * TAB + RIGTH_ANGLE +
-                  VERTICAL_LINE + " " + item.name)
-            level = indentation_level + 1
-            print_directory(item.path, level)
+            directories = directories + 1
+            if (count == ending):
+                drawVerticalLine.insert(indentation_level, False)
+                print(calculateBase(indentation_level) +
+                      RIGTH_ANGLE_LINE +
+                      HORIZONTAL_LINE + item.name)
+            else:
+                print(calculateBase(indentation_level) +
+                      HORIZONTAL_VERTICAL_LINE +
+                      HORIZONTAL_LINE + item.name)
+                drawVerticalLine.insert(indentation_level, True)
+            x, y = print_directory(item.path, indentation_level + 1)
+            directories = directories + x
+            files = files + y
         elif (count == ending):
-            print((indentation_level * HORIZONTAL_LINE) +
-                  (indentation_level * TAB) + RIGTH_ANGLE +
-                  VERTICAL_LINE + " " + item.name)
+            files = files + 1
+            print(calculateBase(indentation_level) +
+                  RIGTH_ANGLE_LINE +
+                  HORIZONTAL_LINE + item.name)
         else:
-            print((indentation_level * HORIZONTAL_LINE) +
-                  indentation_level * TAB + HORIZONTAL_VERTICAL_LINE +
-                  VERTICAL_LINE + " " + item.name)
-            count += 1
+            files = files + 1
+            print(calculateBase(indentation_level) +
+                  HORIZONTAL_VERTICAL_LINE +
+                  HORIZONTAL_LINE + item.name)
+        count += 1
+    return directories, files
 
 
-print_directory(".")
+def calculateBase(indentation_level):
+    result = ""
+    if indentation_level <= 0:
+        return result
+    for i in range(0, indentation_level):
+        if (drawVerticalLine[i]):
+            result = result + VERTICAL_LINE + TAB
+        else:
+            result = result + SPACE + TAB
+    return result
+
+
+directories, files = print_directory(".")
+print()
+print(directories, "directories, ", files, "files")
